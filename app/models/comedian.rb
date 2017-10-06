d33d66a88c9231a6b93116e3e8cf9171ab4ea359
Marshall Houston <marshall.houston@gmail.com>
2017-10-06 10:38:12 -0600

    add average age method and view

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,4 +1,9 @@
 class Comedian < ActiveRecord::Base
   has_many :specials
 
+#As a user, when I visit /comedians I also see the average age for all comedians.
+
+  def average_age
+    Comedian.average(:age).round
+  end
 end
