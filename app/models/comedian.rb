e4909e14c55c46ea57acfdf5f0edee48fd8a382f
Marshall Houston <marshall.houston@gmail.com>
2017-10-06 10:41:33 -0600

    add number of specials for each comedian

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -2,8 +2,14 @@ class Comedian < ActiveRecord::Base
   has_many :specials
 
 #As a user, when I visit /comedians I also see the average age for all comedians.
-
   def average_age
     Comedian.average(:age).round
   end
+
+#As a user, when I visit /comedians I also see the count of specials for each comedian.
+
+  def specific_age(age)
+    Comedian.where(params[age])
+  end
+  
 end
